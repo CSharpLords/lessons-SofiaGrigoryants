a23f29aa5f41c39095e2d2b89fa3d5eeb1b88387
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2017-10-08 16:25:12 +0300

    Баланс

---
+++ 04. switches/баланс/баланс/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваш баланс");
+            int cash = int.Parse(Console.ReadLine());
+
+            int price = 500;
+            int age = 10;
+            int discount = 200;
+            if (age < 9)
+            {
+                price = price - discount;
+            }
+            else
+            {
+                Console.WriteLine("У Вас нет скидки");
+            }
+
+            Console.WriteLine("Ваш баланс " + (cash - price) + " рублей");
+            Console.ReadLine();
+        }
+    }
+}
