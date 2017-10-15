afff5cc9f331f9af5c373e0818d1c3ae83338de0
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2017-10-15 17:08:56 +0300

    нью

---
+++ 04. switches/х/х/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число х");
+
+            int x = int.Parse(Console.ReadLine());
+
+            if (x > 0)
+            {
+                Console.WriteLine("х = 1");
+            }
+            else if (x < 0)
+            {
+                Console.WriteLine("x = -1");
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine("х = 0");
+            }
+            Console.ReadLine();
+        }
+    }
+}
