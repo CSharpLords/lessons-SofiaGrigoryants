113239ca6d2eb5166ee61243c7a4a9bb14b771fa
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2017-11-12 17:10:52 +0300

    новое

---
+++ 09. while/генератор/генератор/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            while (true)
+            {
+                int number = rand.Next(0, 100);
+
+                Console.WriteLine("Запомните число: " + number);
+                Thread.Sleep(300);
+                Console.Clear();
+
+                Console.WriteLine("Введите число:");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("Верно :)");
+                }
+                else
+                {
+                    Console.WriteLine("Неверно :(");
+                }
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+    }
+}
