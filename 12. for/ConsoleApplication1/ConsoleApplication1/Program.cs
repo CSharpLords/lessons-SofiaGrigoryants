9a44e58fe4ba5b1a296763839012ddd9e25bc6d0
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2017-12-03 15:27:58 +0300

    Новое1

---
+++ 12. for/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int time = 1; time <= 9; time = time + 1)
+            {
+                int result = 7 * time;
+                Console.WriteLine(time + " х 7 = " + result);
+                Thread.Sleep(1000);
+            }
+            Console.Read();
+        }
+    }
+}
