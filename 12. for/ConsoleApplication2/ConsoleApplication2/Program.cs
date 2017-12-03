519d88764d98ca3c1320c507dfdca2d8ad679e0e
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2017-12-03 16:54:22 +0300

    new

---
+++ 12. for/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 10; number <= 25; number = number + 1)
+            {
+                double numb = .4;
+                double n = number + numb;
+                Console.WriteLine(number + " " + n);
+                Thread.Sleep(1000);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ 12. for/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int dollar = 1; dollar <= 20; dollar = dollar + 1)
+            {
+                int rub = dollar * 58;
+                Console.WriteLine("В долларах: " + dollar + ". В рублях: " + rub);
+                Thread.Sleep(1000);
+            }
+            Console.Read();
+        }
+    }
+}
