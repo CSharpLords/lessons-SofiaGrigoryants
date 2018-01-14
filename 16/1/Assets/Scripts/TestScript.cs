1528735488b525b5ec289089865f50d0a4d8c534
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2018-01-14 17:06:53 +0300

    Юнити

---
+++ 16/1/Assets/Scripts/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TestScript : MonoBehaviour {
+
+	float time = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time = time + Time.deltaTime;
+		int seconds = (int)time;
+		print ("Time: " + seconds);
+	}
+}
