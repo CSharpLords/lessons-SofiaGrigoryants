b75ae167d6de40c1b9413fc76c552bf708cf2d52
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2018-01-28 17:01:48 +0300

    Юнити

---
+++ 17. unity. conditions/New Unity Project 1/Assets/Scripts/script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+
+	private float max = 0;
+	private float min = 1000;
+
+	void Update () {
+
+		if (transform.position.y > max)
+		{
+			max = transform.position.y;
+		}
+		if (transform.position.y < min) {
+			min = transform.position.y;
+		}
+		print ("Рекорд высоты: " + max + ", Минимальная высота: " + min);
+	}
+}
---
+++ 17. unity. conditions/New Unity Project/Assets/Scripts/EnemyPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPosition : MonoBehaviour {
+	public Transform enemy;
+
+	// Use this for initialization
+	void Start () {
+		print (enemy.position.x);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(enemy.position, transform.position);
+		print (dist);
+	}
+}
