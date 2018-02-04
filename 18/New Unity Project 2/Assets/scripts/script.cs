0bc1292c7417367b4ea014d31fa643bbca68e209
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2018-02-04 17:01:36 +0300

    18

---
+++ 18/New Unity Project 2/Assets/scripts/script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+	float time = 15;
+	public Transform enemy;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time = time - Time.deltaTime;
+		if (time < 0)
+		{
+			Vector3 v = new Vector3(-0.08f, 3.18f, -12.78f);
+			transform.position = v;
+			time = 15;
+		}
+		float dist = Vector3.Distance(enemy.position, transform.position);
+		//print (dist);
+		if (dist < 3) 
+		{
+			print ("Победа! Секретный предмет найден!!!");
+		}
+	}
+}
