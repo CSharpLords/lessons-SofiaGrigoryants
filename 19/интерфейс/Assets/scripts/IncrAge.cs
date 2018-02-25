3406e1597d0c10a01ff7598683ced5e366cd51bf
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2018-02-25 16:44:06 +0300

    21

---
+++ 19/интерфейс/Assets/scripts/IncrAge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IncrAge : MonoBehaviour {
+
+	public Text age;
+	// Use this for initialization
+	void Start () {
+		Button plus = GetComponent<Button> ();
+		plus.onClick.AddListener (Increase);
+	}
+
+	// Update is called once per frame
+	void Increase () {
+		MinAge.number = MinAge.number + 1;
+		age.text = MinAge.number.ToString();
+	}
+}
---
+++ 19/интерфейс/Assets/scripts/MinAge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinAge : MonoBehaviour {
+
+	public Text age;
+	public static float number = 18;
+	// Use this for initialization
+	void Start () {
+		Button plus = GetComponent<Button> ();
+		plus.onClick.AddListener (Increase);
+	}
+
+	// Update is called once per frame
+	void Increase () {
+		number = number - 1;
+		age.text = number.ToString();
+	}
+}
