78214d020ecc5438143f4fdbcc9f7c641704118e
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2018-03-04 16:58:42 +0300

    Football

---
+++ 22. UI and classes/Забей гол!/Assets/menuscrpts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public Button startButton;
+	public Button creditsButton;
+	public Button exitButton;
+
+	// Use this for initialization
+	void Start () {
+		startButton.onClick.AddListener (LoadGame);
+		exitButton.onClick.AddListener (ExitGame);
+	}
+
+	void LoadGame() {
+		SceneManager.LoadScene("Football");
+	}
+		
+	void ExitGame() {
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
