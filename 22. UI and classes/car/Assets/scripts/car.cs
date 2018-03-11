d5d1d2f52f7309ba0638576a177dfd9f011837bc
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2018-03-11 16:47:38 +0300

    Car and football

---
+++ 22. UI and classes/car/Assets/scripts/car.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class car : MonoBehaviour {
+
+	public Button redButton;
+	public Button greenButton;
+	public Button whiteButton;
+	public Button blackButton;
+	public Button blueButton;
+
+	// Use this for initialization
+	void Start () {
+		redButton.onClick.AddListener (PaintRed);
+		greenButton.onClick.AddListener (PaintGreen);
+		whiteButton.onClick.AddListener (PaintWhite);
+		blackButton.onClick.AddListener (PaintBlack);
+		blueButton.onClick.AddListener (PaintBlue);
+	}
+
+	void PaintRed ()
+	{
+		Renderer render = GetComponentInChildren<Renderer>();
+		render.material.color = Color.red;
+	}
+
+	void PaintGreen ()
+	{
+		Renderer render = GetComponentInChildren<Renderer>();
+		render.material.color = Color.green;
+	}
+
+	void PaintWhite ()
+	{
+		Renderer render = GetComponentInChildren<Renderer>();
+		render.material.color = Color.white;
+	}
+
+	void PaintBlack ()
+	{
+		Renderer render = GetComponentInChildren<Renderer>();
+		render.material.color = Color.black;
+	}
+
+	void PaintBlue ()
+	{
+		Renderer render = GetComponentInChildren<Renderer>();
+		render.material.color = Color.blue;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
