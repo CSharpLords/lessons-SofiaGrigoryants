bd2a26fd3349ecef51954eded0da35969e192368
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2018-03-18 17:01:14 +0300

    Hero

---
+++ 22. UI and classes/runner.1/Assets/scripts/HeroCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCollision : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Obstacle") {
+			Destroy (gameObject);
+		}
+	}
+}
