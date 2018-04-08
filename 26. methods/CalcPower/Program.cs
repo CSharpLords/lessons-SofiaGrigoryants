39588a276849d1ba23e54b3999b64bf04faa78a1
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2018-04-08 16:43:54 +0300

    08.04

---
+++ 26. methods/CalcPower/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalcPower {
+	class Program {
+		static void Main(string[] args) {
+			Console.Write("Введите число: ");
+			int n = int.Parse(Console.ReadLine());
+			Console.Write("Введите степень: ");
+			int e = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(n + "^" + e + "=" + Power(n, e));
+			Console.ReadLine();
+		}
+
+		static int Power(int number, int exponent) {
+			int result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result = result * number;
+            }
+            return result;
+		}
+	}
+}
---
+++ 26. methods/PairSymbols/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PairSymbols {
+    class Program {
+        static void Main(string[] args) {
+            CheckPairs("namespace ConsoleApplication1 {class Program {static void Main(string[] args) {}", "{", "}");
+            CheckPairs("graphics = e.Graphics;graphics.SmoothingMode = SmoothingMode.AntiAlias;graphics.Clear(Color.FromArgb(0x44444400));DrawCar(carX, 200);", "(", ")");
+
+            Console.ReadLine();
+        }
+
+        private static void CheckPairs(string code, string open, string closed)
+        {
+            throw new NotImplementedException();
+            int nums = 0;
+            int nums1 = 0;
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (code[i].ToString() == open)
+                {
+                    nums = nums + 1;
+                }
+                if (code[i].ToString() == closed)
+                {
+                    nums1 = nums1 + 1;
+                }
+                if (nums == nums1)
+                {
+                    Console.WriteLine("Всё верно");
+                }
+                else
+                {
+                    Console.WriteLine("Не верно");
+                }
+            }
+        }
+    }
+}
